7438dd256ec3ae102ff2d49f3e7fcdecf1cd8e09
Christina-ZXY <Christinaaaa.mm@gmail.com>
2025-02-26 00:35:07 +1300

    Add sidebar navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -9,11 +22,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"><Topbar /></main>
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+            </Routes>
+
+
+
+
+          </main>
 
         </div>
       </ThemeProvider>
-    </ColorModeContext.Provider>
+    </ColorModeContext.Provider >
   );
 }
 
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,4 +1,12 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import Header from "../../components/Header"
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" >
+        <Header title="DASHBOARD" subtitle="Welcome2" />
+      </Box>
+    </Box>
+  )
 }
 export default Dashboard;
